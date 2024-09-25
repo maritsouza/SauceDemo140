c5ed67b62fefdfc72d1d6e44ce2b998b9470a270
Maria Souza <mariatsouza02@gmail.com>
2024-09-25 15:31:08 +0100

    Verificações da página de inventário

--- test_selecionar_produto_swd.py
+++ test_selecionar_produto_swd.py
@@ -14,11 +14,18 @@ class Teste_Produtos():
         self.driver = webdriver.Edge()                   # Instanciar o objeto do Selenium WebDriver como Edge
         self.driver.implicitly_wait(10)                  # Define o tempo de espera padrão por elementos de 10 segundos
         
-    def teardown_method(self, method):                         # Método de finalização dos testes
+    def teardown_method(self, method):                   # Método de finalização dos testes
         self.driver.quit()                               # Encerra / destrói o objeto do Selenium WebDriver da memória
         
     def test_selecionar_produto(self):                   # Método de teste
         self.driver.get(self.url)                        # Abre o navegador e acessa a URL
-        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")      # Escreve no campo de usuário
-        self.driver.find_element(By.NAME, "password").send_keys("secret_sauce")      # Escreve no campo de senha        
+        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")               # Escreve no campo de usuário
+        self.driver.find_element(By.NAME, "password").send_keys("secret_sauce")               # Escreve no campo de senha
+        self.driver.find_element(By.CSS_SELECTOR, "input.submit-button.btn_action").click()   # Clique no botão de login
         
+        # Transição para a página de produtos
+
+        assert self.driver.find_element(By.CSS_SELECTOR, ".title").text == "Products"               # Confirma se o elemento é "Products"
+        assert self.driver.find_element(By.ID, "item_4_title_link").text == "Sauce Labs Backpack"   # Confirma se o elemento é a mochila
+        # Confirma o preço da mochila
+        assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item:nth-child(1) .inventory_item_price").text == "$29.99"
